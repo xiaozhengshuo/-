c8f7e904a224e8573a15405ef774dac12b4c489b
xiaozhengshuo <57279233+xiaozhengshuo@users.noreply.github.com>
2021-07-18 23:13:33 +0800

    Add files via upload

---
+++ IDAL/IMemberService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Models;
+
+namespace IDAL
+{
+    interface IMemberService
+    {
+        //获取所有社团成员信息
+        DataTable GetAllMembers();
+        //根据成员编号获得社团成员信息
+        ClubMember GetMemberByID(string id);
+        //添加成员
+        bool AddMember(ClubMember member);
+        //修改成员
+        bool UpdateMember(ClubMember member);
+        //根据成员编号删除成员
+        bool DeleteMember(string id);
+
+
+    }
+}
