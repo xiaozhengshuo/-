a6efaa2e4459bbfeac6f698f0ad37316e1ddf981
xiaozhengshuo <57279233+xiaozhengshuo@users.noreply.github.com>
2021-07-18 23:11:59 +0800

    Add files via upload

---
+++ BLL/MemberManage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using DAL;
+using System.Data;
+
+namespace BLL
+{
+    public class MemberManage
+    {
+        public DataTable GetAllMembers()
+        {
+            try
+            {
+                MemberService memberdal = new MemberService();
+                return memberdal.GetAllMembers();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+            
+    }
+}
